c7df8cc88e7ace9e3d8fcc3819e255e81d054773
Sébastien Santoro <dereckson@espace-win.org>
2018-11-04 20:11:30 +0000

    Improve resolution logic and code quality
    This allows resolution for home directories put in a parent directory
    or for /var/wwwroot/<domain>/<sub> with a configuration like this:
    
    ```name=~/.urls.yml
    urls:
    
        /var/home-wwwroot:
          userdir: {}
    
        /var/wwwroot:
          wwwroot: {}
    ```
    
    The codebase adheres to PEP-8 convention.

--- url.py
+++ url.py
@@ -17,9 +17,19 @@
 #   </auto-generated>
 
 
+import platform
 import os
 import sys
 import yaml
+
+
+#   -------------------------------------------------------------
+#   Exceptions
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+class NotFoundException(Exception):
+    pass
 
 
 #   -------------------------------------------------------------
@@ -80,31 +90,78 @@ def get_configuration():
 #   URL resolver
 #   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-def extract_relative_url (base_directory, search_path):
+
+def extract_relative_url(base_directory, search_path):
     n = len(base_directory) + 1
     return search_path[n:]
-    
 
-def resolve_url (base_directory, args, search_path):
+
+def extract_relative_user_url(base_directory, search_path):
+    return extract_relative_url_in_fragments(base_directory,
+                                             search_path, 1)
+
+
+def extract_relative_wwwroot_url(base_directory, search_path):
+    return extract_relative_url_in_fragments(base_directory,
+                                             search_path, 2)
+
+
+def extract_relative_url_in_fragments(base_directory, search_path,
+                                      fragments_count):
+    base_url = extract_relative_url(base_directory, search_path)
+    fragments = base_url.split("/", fragments_count)
+
+    expected_len = fragments_count + 1
+    actual_len = len(fragments)
+    delta_len = expected_len - actual_len
+
+    if delta_len > 1 or len(fragments[0]) == 0:
+        raise NotFoundException()
+
+    if delta_len == 1:
+        fragments.append("")
+
+    return tuple(fragments)
+
+
+def resolve_url(base_directory, args, search_path):
     if 'static' in args:
-        return args['static'] + extract_relative_url(base_directory, search_path)
+        return args['static'] + extract_relative_url(base_directory,
+                                                     search_path)
+
+    if 'userdir' in args:
+        username, local_url = extract_relative_user_url(base_directory,
+                                                        search_path)
+        return "https://" + platform.node() + "/~" + username + "/" + local_url
+
+    if 'wwwroot' in args:
+        domain, sub, local_url = extract_relative_wwwroot_url(base_directory,
+                                                              search_path)
+        return "https://" + sub + "." + domain + "/" + local_url
 
     return None
 
 
-def find_path (base_directory, search_path):
+def find_path(base_directory, search_path):
     if os.path.isabs(search_path):
-        return search_path
+        normalized_path = search_path
+    else:
+        normalized_path = os.path.normpath(os.path.join(base_directory,
+                                                        search_path))
 
-    return os.path.normpath(os.path.join(base_directory, search_path))
+    return os.path.realpath(normalized_path)
 
 
 def find_url(urls, base_directory, required_path):
     path = find_path(base_directory, required_path)
 
     for url_base_dir, url_args in urls.items():
+        url_base_dir = os.path.realpath(url_base_dir)
         if path.startswith(url_base_dir):
-            return resolve_url(url_base_dir, url_args, path)
+            try:
+                return resolve_url(url_base_dir, url_args, path)
+            except NotFoundException:
+                continue
 
     return None
 
